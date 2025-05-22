a8a4f9efa2b71b071aadb514c3930edd1b3830ee
Michel Araujo <michel.araujo.zeca@gmail.com>
2025-05-22 20:01:28 -0300

    `restart`

---
+++ TS/src/nameSpaces/01_basic.ts
@@ -0,0 +1,8 @@
+
+
+namespace MyNameSpace {
+    export const animal = { name: 'Kim', type: 'dog', breed: 'spritz', cor: 'Caramelo' }
+}
+
+// console.log(animal) it`s undefined
+console.log(MyNameSpace.animal)
